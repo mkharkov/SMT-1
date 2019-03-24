46c4f223423652b1a567929428ccbc2f6fccb14a
Mykola Kharkov <Mykola.Kharkov@harmonicinc.com>
2019-03-24 22:00:07 +0200

    Created tabControl and three tabs: Charts, Control, Plan Created GroupBox and Example Lable with Not active textBox

--- SMT-1/MainForm.Designer.cs
+++ SMT-1/MainForm.Designer.cs
@@ -29,22 +29,90 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Plan = new System.Windows.Forms.TabPage();
+            this.Control = new System.Windows.Forms.TabPage();
+            this.Charts = new System.Windows.Forms.TabPage();
+            this.tabControlControlPanel = new System.Windows.Forms.TabControl();
+            this.groupBoxCurrentParameters = new System.Windows.Forms.GroupBox();
+            this.labelT0 = new System.Windows.Forms.Label();
+            this.textBoxT0 = new System.Windows.Forms.TextBox();
+            this.tabControlControlPanel.SuspendLayout();
+            this.groupBoxCurrentParameters.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Plan
+            // 
+            resources.ApplyResources(this.Plan, "Plan");
+            this.Plan.Name = "Plan";
+            this.Plan.UseVisualStyleBackColor = true;
+            // 
+            // Control
+            // 
+            resources.ApplyResources(this.Control, "Control");
+            this.Control.Name = "Control";
+            this.Control.UseVisualStyleBackColor = true;
+            // 
+            // Charts
+            // 
+            resources.ApplyResources(this.Charts, "Charts");
+            this.Charts.Name = "Charts";
+            this.Charts.UseVisualStyleBackColor = true;
+            // 
+            // tabControlControlPanel
+            // 
+            this.tabControlControlPanel.Controls.Add(this.Charts);
+            this.tabControlControlPanel.Controls.Add(this.Control);
+            this.tabControlControlPanel.Controls.Add(this.Plan);
+            resources.ApplyResources(this.tabControlControlPanel, "tabControlControlPanel");
+            this.tabControlControlPanel.Name = "tabControlControlPanel";
+            this.tabControlControlPanel.SelectedIndex = 0;
+            // 
+            // groupBoxCurrentParameters
+            // 
+            this.groupBoxCurrentParameters.Controls.Add(this.textBoxT0);
+            this.groupBoxCurrentParameters.Controls.Add(this.labelT0);
+            resources.ApplyResources(this.groupBoxCurrentParameters, "groupBoxCurrentParameters");
+            this.groupBoxCurrentParameters.Name = "groupBoxCurrentParameters";
+            this.groupBoxCurrentParameters.TabStop = false;
+            // 
+            // labelT0
+            // 
+            resources.ApplyResources(this.labelT0, "labelT0");
+            this.labelT0.Name = "labelT0";
+            // 
+            // textBoxT0
+            // 
+            resources.ApplyResources(this.textBoxT0, "textBoxT0");
+            this.textBoxT0.Name = "textBoxT0";
             // 
             // MainForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.groupBoxCurrentParameters);
+            this.Controls.Add(this.tabControlControlPanel);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControlControlPanel.ResumeLayout(false);
+            this.groupBoxCurrentParameters.ResumeLayout(false);
+            this.groupBoxCurrentParameters.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabPage Plan;
+        private System.Windows.Forms.TabPage Control;
+        private System.Windows.Forms.TabPage Charts;
+        private System.Windows.Forms.TabControl tabControlControlPanel;
+        private System.Windows.Forms.GroupBox groupBoxCurrentParameters;
+        private System.Windows.Forms.TextBox textBoxT0;
+        private System.Windows.Forms.Label labelT0;
     }
 }
 
--- SMT-1/MainForm.cs
+++ SMT-1/MainForm.cs
@@ -16,5 +16,10 @@ namespace SMT_1
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            textBoxT0.Text = "Degrees";
+        }
     }
 }
